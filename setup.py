3917b1546498c3b5ba0b4ca83d6a61ff666de1b5
Georg Brandl <g.brandl@fz-juelich.de>
2021-03-24 19:31:24 +0100

    handle numpy build dependency using pyproject.toml
    Change-Id: I192e1aaba6bb46e944c08570c751bba833cda0a5

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@
 from gitversion import get_version
 version = get_version().lstrip('v')
 
+import numpy
+
 from setuptools import setup, Extension
 
 setup(
@@ -12,6 +14,7 @@ setup(
     description='NICOS quick but restricted YAML dumper module',
     author='Georg Brandl',
     author_email='g.brandl@fz-juelich.de',
-    ext_modules=[Extension('quickyaml', ['quickyaml.c'])],
-    requires=['numpy'],
+    ext_modules=[Extension('quickyaml', ['quickyaml.c'],
+                           include_dirs=[numpy.get_include()])],
+    install_requires=['numpy'],
 )
